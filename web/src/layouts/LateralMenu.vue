7ebceddea8c3613892fa6b63d4fbc64b0ed7b9c7
Leonardo <terlizzileonardo@gmail.com>
2024-07-16 19:57:30 -0300

    changed lateral menu style.

--- web/src/layouts/LateralMenu.vue
+++ web/src/layouts/LateralMenu.vue
@@ -1,23 +1,25 @@
 <template lang="">
-  <div class="flex flex-col justify-center mx-auto py-5 px-2" v-if="isLoaded">
-    <div class="avatar">
-      <div class="w-14 h-14 rounded-full mx-auto" @click="goHome">
-        <img :src="user.avatar" />
-      </div>
-    </div>
-    <div class="divider divider-primary"></div>
-
-    <ul class="flex flex-col gap-4 my-2 mx-auto">
-      <li v-for="project in projects" v-if="isLoaded" @click="goTo(project)">
-        <div class="avatar">
-          <div
-            class="w-14 h-14 rounded-full mx-auto hover:cursor-pointer hover:scale-110 transition-all hover:border-2 hover:border-cyan-200 p-1 hover:border-opacity-60"
-          >
-            <img :src="project.project_avatar" />
-          </div>
+  <div>
+    <div class="flex flex-col justify-center mx-auto py-5 px-2" v-if="isLoaded">
+      <div class="avatar">
+        <div class="w-14 h-14 rounded-full mx-auto" @click="goHome">
+          <img :src="user.avatar" />
         </div>
-      </li>
-    </ul>
+      </div>
+      <div class="divider divider-primary"></div>
+
+      <ul class="flex flex-col gap-4 my-2 mx-auto">
+        <li v-for="project in projects" v-if="isLoaded" @click="goTo(project)">
+          <div class="avatar">
+            <div
+              class="w-14 h-14 rounded-full mx-auto hover:cursor-pointer hover:scale-110 transition-all hover:border-2 hover:border-cyan-200 p-1 hover:border-opacity-60"
+            >
+              <img :src="project.project_avatar" />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
