495530a5bebab4decb55a8862d08017d03de19a1
Leonardo <terlizzileonardo@gmail.com>
2024-07-19 19:01:42 -0300

    added fontawesome icons lib

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,11 +3,25 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import {
+  faUserPlus,
+  faUserMinus,
+  faCirclePlus,
+  faFolderTree,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserPlus, faUserMinus, faCirclePlus, faFolderTree, faPenToSquare);
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(router);
 app.use(pinia);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
